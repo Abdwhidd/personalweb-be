a931ed74936e1a1e366f31346aaf1f8b34e95aec
abdwhidd <abdulwahidkaharr@gmail.com>
2025-05-18 20:19:27 +0700

    fix/Dockerfile: update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-# Pastikan .env tersedia
-if [ ! -f .env ]; then
-  cp .env.example .env
+# Pastikan APP_KEY ada dari ENV Railway
+if [ -z "$APP_KEY" ]; then
+  echo "❌ APP_KEY is missing from environment!"
+  exit 1
 fi
+
+echo "APP_KEY: $APP_KEY"
 
 # Laravel setup
 composer install --optimize-autoloader
-php artisan key:generate --force
+php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
@@ -15,5 +18,5 @@ php artisan migrate --force
 php artisan storage:link || true
 
 # Start server
-echo "Starting server on port ${PORT:-8080}"
+echo "✅ Starting server on port ${PORT:-8080}"
 php -S 0.0.0.0:${PORT:-8080} -t public
